61da88785406f238565b157cf1dfed7bf01b437d
Shamol Mojumder <shamolmojumder14@gmail.com>
2020-10-12 21:46:10 +0600

    Update reduce.js

--- Advanced JavaScript, Javascript Interview Questions/reduce.js
+++ Advanced JavaScript, Javascript Interview Questions/reduce.js
@@ -20,3 +20,19 @@ console.log(max)
 
 const max=nums.reduce((acc,val)=>Math.max(acc,val))
 console.log(max)
+
+
+const prodcts=[
+  {name:"pen",price:5,amount:2},
+  {name:"book",price:25,amount:5},
+  {name:"laptop",price:100,amount:1}
+]
+
+const sum=prodcts.reduce((acc,val)=>{
+return acc+val.price*val.amount
+
+},0)
+
+console.log(sum)
+
+
